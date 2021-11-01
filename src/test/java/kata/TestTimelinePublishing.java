43b6e6c53c8e464a09ee60f909ce0ae733f953f6
Gentry Tran <gentry.lu@gmail.com>
2021-11-01 14:48:45 -0400

    First passing test. Creating a user object.

--- src/test/java/kata/TestTimelinePublishing.java
+++ src/test/java/kata/TestTimelinePublishing.java
@@ -1,5 +1,14 @@
 package kata;
 
-public class TestTimelinePublishing {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.Test;
+
+public class TestTimelinePublishing {
+	
+	@Test
+	public void newUserReturnsName() {
+		User user = new User("Alice");
+		assertEquals("Alice", user.getName());
+	}
 }
